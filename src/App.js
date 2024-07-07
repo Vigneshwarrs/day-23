7886db1613222be02a5e89b433435b9d7beaeca6
Vigneshwarrs <vigneshwarrs@outlook.com>
2024-07-07 20:52:51 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,60 +1,55 @@
 import './App.css';
-import { Container, Navbar } from 'react-bootstrap';
+import { Container, Navbar, Card, Button, Row, Col } from 'react-bootstrap';
 import { useState } from 'react';
 
 function App() {
   const products = [{
     title: "iphone 15 pro",
     rating: "⭐⭐⭐⭐",
-    Original_price: "₹ 1,34,000",
-    Actual_price: "₹ 1,24,000"
-  },{
+    original_price: "₹ 1,34,000",
+    actual_price: "₹ 1,24,000"
+  }, 
+  {
     title: "Samsaung s24 ultra",
     rating: "⭐⭐⭐⭐",
-    Original_price: "₹ 1,29,000",
-    Actual_price: "₹ 1,24,000"
-  },
-  {
-    title: "Samsung s24",
-    rating: "⭐⭐⭐⭐",
-    Original_price: "₹ 79,999",
-    Actual_price: "₹ 74,999"
+    original_price: "₹ 1,29,000",
+    actual_price: "₹ 1,24,000"
   },
   {
     title: "iQoo 12 5G",
     rating: "⭐⭐⭐⭐",
-    Original_price: "₹ 59,000",
-    Actual_price: "₹ 52,999"
+    original_price: "₹ 59,000",
+    actual_price: "₹ 52,999"
   },
   {
     title: "Vivo x100",
     rating: "⭐⭐⭐⭐",
-    Original_price: "₹ 75,000",
-    Actual_price: "₹ 69,999"
+    original_price: "₹ 75,000",
+    actual_price: "₹ 69,999"
   },
   {
     title: "Realme GT 6",
     rating: "⭐⭐⭐⭐",
-    Original_price: "₹ 51,999",
-    Actual_price: "₹ 44,999"
+    original_price: "₹ 51,999",
+    actual_price: "₹ 44,999"
   },
   {
     title: "Nothing 2",
     rating: "⭐⭐⭐⭐",
-    Original_price: "₹ 59,999",
-    Actual_price: "₹ 39,999"
+    original_price: "₹ 59,999",
+    actual_price: "₹ 39,999"
   },
   {
     title: "CMF 1",
     rating: "⭐⭐⭐⭐",
-    Original_price: "₹ 25,000",
-    Actual_price: "₹ 19,999"
+    original_price: "₹ 25,000",
+    actual_price: "₹ 19,999"
   },
   {
     title: "Xiaomi 14 civi",
     rating: "⭐⭐⭐⭐",
-    Original_price: "₹ 54,999",
-    Actual_price: "₹ 42,999"
+    original_price: "₹ 54,999",
+    actual_price: "₹ 42,999"
   }]
   return (
     <div className="App">
@@ -64,19 +59,48 @@ function App() {
 
 }
 
-function Cards(obj) {
+function Cards({ obj }) {
   const [cart, setCart] = useState(0);
 
 
+  function handleCart(e) {
+    if (e.target.textContent.includes("Add")) {
+      setCart(cart + 1);
+      e.target.textContent = "Remove form Cart";
+    } else if (e.target.textContent.includes("Remove")) {
+      setCart(cart - 1);
+      e.target.textContent = "Add to cart";
+    }
+  }
+
   return (
     <div>
-      <Container>
-      <Navbar bg="dark" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand href="#home">Mobile Market</Navbar.Brand>
-          <Navbar.Text>Carts: {cart}</Navbar.Text>
-        </Container>
-      </Navbar>
+        <Navbar bg="dark" data-bs-theme="dark">
+          <Container>
+            <Navbar.Brand >Mobile Market</Navbar.Brand>
+            <Navbar.Text>Carts: {cart}</Navbar.Text>
+          </Container>
+        </Navbar>
+        <Container className='px-4 px-lg-5 mt-5'>
+        <Row className='gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4'>
+          {
+            obj.map((o) => {
+              return (
+                <Col className='mb-5'>
+                  <Card>
+                    <Card.Img variant="top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" />
+                    <Card.Body>
+                      <Card.Title>{o.title}</Card.Title>
+                      <Card.Text>{o.rating}</Card.Text>
+                      <Card.Text><span style={{ textDecoration: "line-through" }}>{o.orignal_price}</span>{o.actual_price}</Card.Text>
+                      <Button variant="primary" onClick={(e) => handleCart(e)}>Add to Card</Button>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              )
+            })
+          }
+        </Row>
       </Container>
     </div>
   );
